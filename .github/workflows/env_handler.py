d842bd802d2f9f3380f81b19313e37f59dcc3ddf
Divya Prakash <47500932+rkisdp@users.noreply.github.com>
2021-08-01 21:19:00 +0530

    Create env_handler.py

---
+++ .github/workflows/env_handler.py
@@ -0,0 +1,9 @@
+# I know this is dirty work but this will be changed if something else found
+import json
+f = open('../../env.json')
+data = json.load(f)
+f.close()
+a = open("../../.env", "w")
+for i in data:
+    a.write(f"{i.get('name')}={i.get('value')}\n")
+a.close()
