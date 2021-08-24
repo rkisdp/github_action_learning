04e460f56313b130b737ac14474ed3b0e176b905
Divya Prakash <rkisdp@gmail.com>
2021-08-25 02:42:55 +0530

    fix for alb

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name="celery_test_task"),
+    path('', views.testv2, name="celery_test_task"),
+    path('accounts/', views.test, name="celery_test_task"),
 ]
