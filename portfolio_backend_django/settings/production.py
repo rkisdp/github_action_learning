4abaef239caa7d049df5f05f92819857f0511224
Divya Prakash <rkisdp@gmail.com>
2021-08-25 12:47:05 +0530

    aws implementation

---
+++ portfolio_backend_django/settings/production.py
@@ -0,0 +1,11 @@
+from decouple import config
+
+AWS_QUERYSTRING_AUTH = config('AWS_ACCESS_KEY_ID', cast=bool)
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = config('AWS_REGION')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
