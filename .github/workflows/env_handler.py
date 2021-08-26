eeaf3bb96fa17658a443a23583e0e84250e4fa0a
Divya Prakash <47500932+rkisdp@users.noreply.github.com>
2021-08-26 23:57:56 +0530

    Delete env_handler.py

--- .github/workflows/env_handler.py
+++
@@ -1,9 +0,0 @@
-# I know this is dirty work but this will be changed if something else found
-import json
-f = open('../../env.json')
-data = json.load(f)
-f.close()
-a = open("../../.env", "w")
-for i in data:
-    a.write(f"{i.get('name')}={i.get('value')}\n")
-a.close()
