c13b6f3ba6316d72c3467c83515e6f0ef2eef123
Divya Prakash <rkisdp@gmail.com>
2021-10-06 00:05:17 +0530

    Added testimonial api and model

--- portfolio/models/__init__.py
+++ portfolio/models/__init__.py
@@ -2,3 +2,4 @@ from .testimonials import Testimonial
 from .projects import Project
 from .skills import Skill
 from .rolls import Role
+from .timeline import Timeline
--- utils/messages.py
+++ utils/messages.py
@@ -2,5 +2,8 @@
 # python imports
 from __future__ import unicode_literals
 
-EMAIL_TO_REQUIRED = 'email-id is required to send email'
-EMAIL_SEND_FAIL = 'attempt to send email failed'
+# lib imports
+from django.utils.translation import ugettext_lazy as _
+
+EMAIL_TO_REQUIRED = _('email-id is required to send email')
+EMAIL_SEND_FAIL = _('attempt to send email failed')
