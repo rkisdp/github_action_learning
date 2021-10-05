79c816a67d8472c71b219fbecce19dc9a7e41762
Divya Prakash <rkisdp@gmail.com>
2021-10-05 12:21:16 +0530

    Change in API and constancy

--- common/gateways/notification.py
+++ common/gateways/notification.py
@@ -7,3 +7,7 @@ from common.models import Notification as NotificationModel
 
 def get_notification(pk: int, user_id: int, type: str):
     return NotificationModel.objects.get(pk=pk, user_id=user_id, type=type)
+
+
+def filter_by_user_and_type(user_id: int, type: str):
+    return NotificationModel.objects.filter_by_user_and_type(user_id=user_id, type=type)
